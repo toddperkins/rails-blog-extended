f1f760d7bf506850608c113718bbcc48cfac6ba3
Todd Perkins <fullstacktodd@gmail.com>
2023-01-20 20:51:17 -0500

    Prior to comments update

--- Gemfile
+++ Gemfile
@@ -34,6 +34,7 @@ gem "bootstrap"
 group :development, :test do
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
   gem "rspec-rails"
+  gem "rails-controller-testing"
   gem "factory_bot_rails"
   gem "faker"
 end
---
+++ spec/requests/article_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe ArticlesController, type: :request do
+  
+  describe "GET index" do
+
+    it "assigns @articles" do
+      article = Article.create
+      get '/articles'
+      expect(assigns(:articles)).to eq([article])
+    end
+
+    it "renders the index template" do
+      get '/articles'
+      expect(response).to render_template("index")
+    end
+
+  end
+
+end
